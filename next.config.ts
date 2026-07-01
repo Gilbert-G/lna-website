50ecce7ae1a14496322a0904ed2996e9b8cffff7
Gilbert-G <Gilbert-G@users.noreply.github.com>
2026-07-01 14:27:43 +0530

    fix: add basePath support for sub-path deployment

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || undefined,
 };
 
 export default withNextIntl(nextConfig);
